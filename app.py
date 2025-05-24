686b2639f48593c636f89ee535dc27779a3dbdc8
Ansh Rastogi <thisisanshrastogi@gmail.com>
2025-05-24 19:32:42 +0000

    change threshold

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from datetime import datetime, timedelta
 def fetch_reviews(app_name):
     try:
         APP_ID = app_name
-        timethreshold=datetime.now() - timedelta(days=180)
+        timethreshold=datetime.now() - timedelta(days=200)
 
         result, _ = reviews(APP_ID, lang="en", country="us", sort=Sort.NEWEST, count=5000)
         filtered  = [r for r in result if r['at']>=timethreshold]
@@ -43,16 +43,8 @@ def main():
             st.write(f"Fetching Reviews for {app_name}")
             app_name = app_name.strip().lower()
             reviews = fetch_reviews(app_name)
+
             if not reviews.empty:
-                data=[]
-                for index, row in reviews.iterrows():
-                    data.append({
-                        'App Name': app_name,
-                        'Review': row['content'],
-                        'Rating': row['score'],
-                        'Date': row['at'],
-                        'User': row['userName']
-                    })
                 st.success(f"✅ Successfully fetched {len(reviews)} reviews for {app_name} with last review on {reviews['at'].min().strftime('%d-%m-%Y')}")
                 st.dataframe(reviews)
             else:
